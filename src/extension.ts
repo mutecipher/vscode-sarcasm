d85c105152c96bc9c54f75054a38395815ad5e8b
Cory Hutchison <cory_hutchison@icloud.com>
2022-02-21 14:17:36 -0700

    Add sarcasm

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,35 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-sarcasm" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('vscode-sarcasm.addSarcasm', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-sarcasm!');
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			const document = editor.document;
+			let selection = editor.selection;
+
+			let word: string;
+
+			if (selection.isEmpty) {
+				selection = new vscode.Selection(0, 0, document.lineCount, 0);
+			}
+
+			word = document.getText(selection);
+
+			editor.edit(editBuilder => {
+				editBuilder.replace(selection, injectSarcasm(word));
+			});
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+function injectSarcasm(text: string): string {
+	let stringLength = text.length;
+	let sarcastic: string = "";
+	for (let i = 0; i < stringLength; i++) {
+		sarcastic = sarcastic.concat(i % 2 === 0 ? text.charAt(i) : text.charAt(i).toUpperCase());
+	}
+	return sarcastic;
+}
